6cead07b59536c852d0305d133da1b81d893a083
Lvaho <329224911@qq.com>
2022-01-23 11:42:56 +0800

    NewMessagesender

---
+++ src/main/java/com/zjc/seckilldemo/rocketmq/NewMessageSender.java
@@ -0,0 +1,16 @@
+package com.zjc.seckilldemo.rocketmq;
+
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewMessageSender {
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+    public void SendMessage(String msg){
+        rocketMQTemplate.convertAndSend("TopicTest",msg);
+        System.out.println("sendMsg"+msg);
+    }
+}
